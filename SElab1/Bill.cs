157f1e0251c64f3a96386035f4bd75a5a07d5838
arsen4ik98 <arsenbeglaryan98@gmail.com>
2017-10-07 11:17:12 +0300

    кинул

---
+++ SElab1/Bill.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SElab1
+{
+    public class Bill
+    {
+        private List<Item> _items;
+        private Customer _customer;
+        public Bill(Customer customer)
+        {
+            this._customer = customer;
+            this._items = new List<Item>();
+        }
+
+        public void addGoods(Item arg)
+        {
+            _items.Add(arg);
+        }
+
+        public string GetHeader()
+        {
+            string caption = "Счет для " + _customer.getName() + "\n" + "\t" + "Название" + "\t" + "Цена" +
+            "\t" + "Кол-во" + "Стоимость" + "\t" + "Скидка" +
+            "\t" + "Сумма" + "\t" + "Бонус" + "\n";
+            return caption;
+        }
+
+        public string GetFooter(double totalAmount, int totalBonus)
+        {
+            string continuum =  "Сумма счета составляет " + totalAmount.ToString() + "\n" + "Вы заработали " + totalBonus.ToString() + " бонусных балов";
+            return continuum;
+        }
+
+        public string GetItemString(double thisAmount, double discount, int bonus, Item each)
+        {
+            string getitem = "\t" + each.getGoods().getTitle() + "\t" +
+                "\t" + each.getPrice() + "\t" + each.getQuantity() +
+                "\t" + GetSum(each).ToString() +
+                "\t" + discount.ToString() + "\t" + thisAmount.ToString() +
+                "\t" + bonus.ToString() + "\n";
+            return getitem;
+        }
+
+        public double GetDiscount(Item each)
+        {
+            double discount = 0;
+            switch (each.getGoods().getPriceCode())
+            {
+                case Goods.REGULAR:
+                    if (each.getQuantity() > 2)
+                        discount = GetSum(each) * 0.03; // 3%
+                    break;
+                case Goods.SPECIAL_OFFER:
+                    if (each.getQuantity() > 10)
+                        discount = GetSum(each) * 0.005; // 0.5%
+                    break;
+                case Goods.SALE:
+                    if (each.getQuantity() > 3)
+                        discount = GetSum(each) * 0.01; // 0.1%
+                    break;
+            }
+           
+            return discount;
+        }
+
+        public int GetBonus(Item each)
+        {
+            int bonus = 0;
+            switch (each.getGoods().getPriceCode())
+            {
+                case Goods.REGULAR:
+                    bonus = (int)(GetSum(each) * 0.05);
+                    break;
+                case Goods.SALE:
+                    bonus = (int)(GetSum(each) * 0.01);
+                    break;
+            }
+            return bonus;
+        }
+
+        public double GetSum(Item each)
+        {
+            double getsum = each.getQuantity() * each.getPrice();
+            return getsum;
+        }
+
+        public double GetUsedBonus(Item each, double thisAmount, double discount)
+        {
+            double usedBonus = 0;
+            if ((each.getGoods().getPriceCode() == Goods.REGULAR) && each.getQuantity() > 5)
+            {
+                usedBonus += _customer.useBonus((int)(thisAmount - discount));
+            }
+            if ((each.getGoods().getPriceCode() == Goods.SPECIAL_OFFER) && each.getQuantity() > 1)
+            {
+                usedBonus += _customer.useBonus((int)(thisAmount - discount));
+            }
+            return usedBonus;
+        }
+
+        public String statement()
+        {
+            double totalAmount = 0;
+            int totalBonus = 0;
+            List<Item>.Enumerator items = _items.GetEnumerator();
+            string result = GetHeader();
+            while (items.MoveNext())
+            {
+                double sumWithDiscount = 0;
+                double usedBonus = 0;
+                double thisAmount = 0;
+                Item each = (Item)items.Current;
+                //определить сумму для каждой строки
+                double discount = GetDiscount(each);
+                int bonus = GetBonus(each);
+                // сумма
+                sumWithDiscount = GetSum(each) - discount;
+                usedBonus = GetUsedBonus(each, sumWithDiscount, discount);
+                thisAmount = sumWithDiscount - usedBonus;
+                //показать результаты
+                result += GetItemString(thisAmount, discount, bonus, each);
+                totalAmount += thisAmount;
+                totalBonus += bonus;
+            }
+            //добавить нижний колонтитул
+            result += GetFooter(totalAmount, totalBonus);
+            //Запомнить бонус клиента
+            _customer.receiveBonus(totalBonus);
+            return result;
+        }
+    }
+}
---
+++ SElab1_Test/SElab1_Test.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using SElab1;
+
+namespace SElab1_Test
+{
+    [TestFixture]
+    public class SElab1_Test
+    {
+        [Test()]
+        public void Cola_Test()
+        {
+            Goods cola = new Goods("Cola", Goods.REGULAR);
+            Item i1 = new Item(cola, 6, 65);
+            Customer x = new Customer("test", 10);
+            Bill b1 = new Bill(x);
+            b1.addGoods(i1);
+            string actual = b1.statement();
+            string expected = "Счет для test\n\tНазвание\tЦена\tКол-воСтоимость\tСкидка\tСумма\tБонус\n\tCola\t\t65\t6\t390\t11,7\t368,3\t19\nСумма счета составляет 368,3\nВы заработали 19 бонусных балов";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test()]
+        public void Pepsi_Test()
+        {
+            Goods pepsi = new Goods("Pepsi", Goods.REGULAR);
+            Item i1 = new Item(pepsi, 3, 50);
+            Customer x = new Customer("test", 10);
+            Bill b1 = new Bill(x);
+            b1.addGoods(i1);
+            string actual = b1.statement();
+            string expected = "Счет для test\n\tНазвание\tЦена\tКол-воСтоимость\tСкидка\tСумма\tБонус\n\tPepsi\t\t50\t3\t150\t4,5\t145,5\t7\nСумма счета составляет 145,5\nВы заработали 7 бонусных балов";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test()]
+        public void Fanta_Test()
+        {
+            Goods fanta = new Goods("Fanta", Goods.REGULAR);
+            Item i1 = new Item(fanta, 1, 35);
+            Customer x = new Customer("test", 10);
+            Bill b1 = new Bill(x);
+            b1.addGoods(i1);
+            string actual = b1.statement();
+            string expected = "Счет для test\n\tНазвание\tЦена\tКол-воСтоимость\tСкидка\tСумма\tБонус\n\tFanta\t\t35\t1\t35\t0\t35\t1\nСумма счета составляет 35\nВы заработали 1 бонусных балов";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test()]
+        public void Cola_Pepsi_Test()
+        {
+            Goods cola = new Goods("Cola", Goods.REGULAR);
+            Goods pepsi = new Goods("Pepsi", Goods.SALE);
+            Item i1 = new Item(cola, 6, 65);
+            Item i2 = new Item(pepsi, 3, 50);
+            Customer x = new Customer("test", 10);
+            Bill b1 = new Bill(x);
+            b1.addGoods(i1);
+            b1.addGoods(i2);
+            string actual = b1.statement();
+            string expected = "Счет для test\n\tНазвание\tЦена\tКол-воСтоимость\tСкидка\tСумма\tБонус\n\tCola\t\t65\t6\t390\t11,7\t368,3\t19\n\tPepsi\t\t50\t3\t150\t0\t150\t1\nСумма счета составляет 518,3\nВы заработали 20 бонусных балов";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test()]
+        public void Cola_Fanta_Test()
+        {
+            Goods cola = new Goods("Cola", Goods.REGULAR);
+            Goods fanta = new Goods("Fanta", Goods.SALE);
+            Item i1 = new Item(cola, 6, 65);
+            Item i2 = new Item(fanta, 1, 35);
+            Customer x = new Customer("test", 10);
+            Bill b1 = new Bill(x);
+            b1.addGoods(i1);
+            b1.addGoods(i2);
+            string actual = b1.statement();
+            string expected = "Счет для test\n\tНазвание\tЦена\tКол-воСтоимость\tСкидка\tСумма\tБонус\n\tCola\t\t65\t6\t390\t11,7\t368,3\t19\n\tFanta\t\t35\t1\t35\t0\t35\t0\nСумма счета составляет 403,3\nВы заработали 19 бонусных балов";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test()]
+        public void Pepsi_Fanta_Test()
+        {
+            Goods pepsi = new Goods("Pepsi", Goods.REGULAR);
+            Goods fanta = new Goods("Fanta", Goods.SALE);
+            Item i1 = new Item(pepsi, 3, 50);
+            Item i2 = new Item(fanta, 1, 35);
+            Customer x = new Customer("test", 10);
+            Bill b1 = new Bill(x);
+            b1.addGoods(i1);
+            b1.addGoods(i2);
+            string actual = b1.statement();
+            string expected = "Счет для test\n\tНазвание\tЦена\tКол-воСтоимость\tСкидка\tСумма\tБонус\n\tPepsi\t\t50\t3\t150\t4,5\t145,5\t7\n\tFanta\t\t35\t1\t35\t0\t35\t0\nСумма счета составляет 180,5\nВы заработали 7 бонусных балов";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test()]
+        public void Cola_Pepsi_Fanta_Test()
+        {
+            Goods cola = new Goods("Cola", Goods.REGULAR);
+            Goods pepsi = new Goods("Pepsi", Goods.REGULAR);
+            Goods fanta = new Goods("Fanta", Goods.SALE);
+            Item i1 = new Item(cola, 6, 65);
+            Item i2 = new Item(pepsi, 3, 50);
+            Item i3 = new Item(fanta, 1, 35);
+            Customer x = new Customer("test", 10);
+            Bill b1 = new Bill(x);
+            b1.addGoods(i1);
+            b1.addGoods(i2);
+            b1.addGoods(i3);
+            string actual = b1.statement();
+            string expected = "Счет для test\n\tНазвание\tЦена\tКол-воСтоимость\tСкидка\tСумма\tБонус\n\tCola\t\t65\t6\t390\t11,7\t368,3\t19\n\tPepsi\t\t50\t3\t150\t4,5\t145,5\t7\n\tFanta\t\t35\t1\t35\t0\t35\t0\nСумма счета составляет 548,8\nВы заработали 26 бонусных балов";
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
